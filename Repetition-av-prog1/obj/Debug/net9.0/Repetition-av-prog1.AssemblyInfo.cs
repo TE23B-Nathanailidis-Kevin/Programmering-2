067aa0a834c5f0d619abc24b0c518907c9aebd14
kevin nathanailidis <kevin.nathanailidis@elev.ga.ntig.se>
2025-08-28 12:07:06 +0200

    kosdkf

--- Repetition-av-prog1/obj/Debug/net9.0/Repetition-av-prog1.AssemblyInfo.cs
+++ Repetition-av-prog1/obj/Debug/net9.0/Repetition-av-prog1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Repetition-av-prog1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5cf860f1818e89973f8e69aa620346eed0dd5c91")]
 [assembly: System.Reflection.AssemblyProductAttribute("Repetition-av-prog1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Repetition-av-prog1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
